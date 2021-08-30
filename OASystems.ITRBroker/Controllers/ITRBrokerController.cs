1ac527ffe4f200acae56e7d55e3de194527be5ac
Ken Lim <ken.lim@oasystems.co.nz>
2021-08-30 16:23:26 +1200

    Removed pre-built api controller

---
+++ OASystems.ITRBroker/Controllers/ITRBrokerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OASystems.ITRBroker.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ITRBrokerController : ControllerBase
+    {
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet]
+        //public void Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpPost]
+        public string Post([FromBody] Test body)
+        {
+            TestAPI.DoTest(body.name);
+
+            //Worker = Worker as 
+
+            return body.name;
+        }
+    }
+
+    public class Test
+    {
+        public string name { get; set; }
+    }
+}
